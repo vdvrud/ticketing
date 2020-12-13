73a0dc326968a6afe5609f11716b4e2c89724ac9
vdvrud <dharmiksoni838@gmail.com>
2020-12-13 22:12:38 +0530

    This is definitely the final one

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -49,7 +49,7 @@ userSchema.pre('save', async function(done) {
     const hashed = await Password.toHash(this.get('password'));
     this.set('password', hashed);
   }
-  done(new Error(''));
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
